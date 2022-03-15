346ec73865ee7d313427d47a32887daf8eb1225b
deeysi <daisysc40@gmail.com>
2022-03-15 00:44:15 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "putchar"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *putchar;
+
+	for (putchar = "putchar\n"; *putchar != '\0'; ++putchar)
+		_putchar(*putchar);
+	return (0);
+}
